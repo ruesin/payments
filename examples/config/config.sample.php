36e8662e72445fbaf7b8c13293c618b33fa9daae
Ruesin <ruesin@163.com>
2016-10-21 15:05:33 +0800

    调整支付示例入口、增加移动支付测试

--- examples/config/config.sample.php
+++ examples/config/config.sample.php
@@ -1,8 +1,17 @@
 <?php
 return array(
     'alipay' => array(
-        'notify_url' => 'http://local.payments.com/alipay/notify.php',
-        'return_url' => 'http://local.payments.com/alipay/return.php',
+        'notify_url' => 'http://local.payments.com/notify.php/alipay',
+        'return_url' => 'http://local.payments.com/return.php/alipay',
+        'partner'    => '2088123456789012',
+        'input_charset'  => 'utf-8',
+        'sign_type'      => 'MD5',
+        'md5_key'        => 'abcdefghijklmnopqrstuvwxyz123456',
+        'cacert'         => TEST_PATH.'alipay/config/cacert.pem',
+    ),
+    'alipay' => array(
+        'notify_url' => 'http://local.payments.com/notify.php/malipay',
+        'return_url' => 'http://local.payments.com/return.php/malipay',
         'partner'    => '2088123456789012',
         'input_charset'  => 'utf-8',
         'sign_type'      => 'MD5',
---
+++ examples/payapi.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>在线支付</title>
+</head>
+<body>
+	<div style="width: 600px; margin: 0 auto">
+		<form action="./submit.php" method="post">
+			<table>
+				<tr>
+					<th>支付单号：</th>
+					<td><input type="text" name="order[out_trade_no]" value="<?=time()?>" /></td>
+					<th>支付金额：</th>
+					<td><input type="text" name="order[money]" value="0.01" /></td>
+				</tr>
+				<tr>
+					<th>商品名称：</th>
+					<td><input type="text" name="order[name]" value="支付测试" /></td>
+					<th>商品描述：</th>
+					<td><input type="text" name="order[desc]" value="支付测试描述" /></td>
+				</tr>
+				<tr>
+					<th>支付方式：</th>
+					<td><input type="text" name="payment" value="<?=$_GET['pay']?>" readonly /></td>
+					<th></th>
+					<td></td>
+				</tr>
+    
+                <?php if($_GET['pay'] == 'malipay'):?>
+                <tr>
+                 <th>商品展示地址：</th>
+                 <td><input type="text" name="order[show_url]" value="http://local.test.com" /></td>
+                 <th></th>
+                 <td></td>
+                </tr>
+                <?php endif;?>
+                
+				<tr>
+					<td colspan="4" align="center"><button>支付</button></td>
+				</tr>
+			</table>
+		</form>
+	</div>
+</body>
+</html>
