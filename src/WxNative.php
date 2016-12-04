a6771118607ae94e9f11ed010d1326ffb9f54951
Ruesin <ruesin@163.com>
2016-12-04 13:18:22 +0800

    clear computer back file

--- src/WxNative.php
+++ src/WxNative.php
@@ -7,7 +7,9 @@ class WxNative extends PayBase
 {
 
     const UNIFIED_ORDER_URL = 'https://api.mch.weixin.qq.com/pay/unifiedorder';
-
+    
+    const QUERY_ORDER_URL = 'https://api.mch.weixin.qq.com/pay/orderquery';
+    
     private $config = [];
 
     public function __construct($params = [])
@@ -16,35 +18,34 @@ class WxNative extends PayBase
     }
 
     /*
-     *
+     * 
      * @see https://pay.weixin.qq.com/wiki/doc/api/native.php?chapter=9_1
+     *
      */
     public function getPayForm($order, $params = [])
     {
         $form = array(
-            'appid' => $this->config['appid'],
+            'appid'  => $this->config['appid'],
             'mch_id' => $this->config['mch_id'],
-            
-            // 'device_info' => 'WEB',//终端设备号(门店号或收银设备ID)，注意：PC网页或公众号内支付请传"WEB"
-            'nonce_str' => StringUtils::createNonceString(), // 随机字符串，不长于32位。
-            'sign' => '', // 签名
-            'body' => $order['subject'],
-            // 'detail' => '',// json 格式商品详情
-            // 'attach' => '微信扫码支付订单。', //附加数据，在查询API和支付通知中原样返回，该字段主要用于商户携带订单的自定义数据
-            'out_trade_no' => $order['out_trade_no'], // 商户系统内部的订单号,32个字符内、可包含字母
-                                                      
-            // 'fee_type' => 'CNY', //货币类型
-            'total_fee' => ceil($order['money'] * 100), // 订单总金额，单位为分
-            'spbill_create_ip' => $_SERVER['REMOTE_ADDR'], // APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP。
-            'time_start' => date('YmdHis'), // 订单生成时间，格式为yyyyMMddHHmmss
-            'time_expire' => date('YmdHis', time() + 7200), // 订单失效时间，格式为yyyyMMddHHmmss 最短失效时间间隔必须大于5分钟
-                                                         // 'goods_tag' => 'test', //商品标记，代金券或立减优惠功能的参数
-            'notify_url' => $this->config['notify_url'], // 接收微信支付异步通知回调地址，通知url必须为直接可访问的url，不能携带参数。
-            'trade_type' => 'NATIVE', // 交易类型
+            'device_info' => 'WEB',
+            'nonce_str' => StringUtils::createNonceString(),
+            'sign'   => '',
+            'body'   => $order['subject'],
+            'detail' => $params['order_detail'],
+            'attach' => $params['order_attach'],
+            'out_trade_no' => $order['out_trade_no'],
+            'fee_type' => 'CNY',
+            'total_fee' => ceil($order['money'] * 100),
+            'spbill_create_ip' => $_SERVER['REMOTE_ADDR'],
+            'time_start' => date('YmdHis'),
+            'time_expire' => date('YmdHis', time() + 7200),
+            //'goods_tag' => 'test',
+            'notify_url' => $this->config['notify_url'],
+            'trade_type' => 'NATIVE',
             'product_id' => $order['order_id'],
-            // trade_type=NATIVE，此参数必传。此id为二维码中包含的商品ID，商户自行定义
             // 'limit_pay' => 'no_credit', //no_credit--指定不能使用信用卡支付
         );
+        
         $result = $this->unifiedOrder($form);
         if (! $result)
             return '';
@@ -55,43 +56,49 @@ class WxNative extends PayBase
         return '<img src="http://paysdk.weixin.qq.com/example/qrcode.php?data=' . urlencode($result["code_url"]) . '"/>';
     }
 
-    protected function unifiedOrder($para_temp, $timeOut = 6)
+    protected function unifiedOrder($params, $timeOut = 6)
     {
-        $para_filter = StringUtils::paraFilter($para_temp, array('sign'));
-        
-        $para_sort = StringUtils::argSort($para_filter);
-        
-        $para_sort['sign'] = $this->buildRequestMysign($para_sort);
-        
-        $xml = StringUtils::arrayToXml($para_sort);
-        
-        // $startTimeStamp = self::getMillisecond();//请求开始时间
+        $params['sign'] = $this->buildRequestMysign($params);
+        $xml = StringUtils::arrayToXml($params);
         $response = $this->postXmlCurl($xml, self::UNIFIED_ORDER_URL);
         $result = StringUtils::XmlToArray($response);
-        
         if ($result['return_code'] != 'SUCCESS') {
             return false;
         }
-        
-        // 验签
-        // ..
-        
-        // self::reportCostTime($url, $startTimeStamp, $result);//上报请求花费时间
+        if (! $this->CheckSign($result)) {
+            return false;
+        }
         return $result;
     }
 
     /**
      * 签名
-     *
-     * @author Ruesin
      */
-    public function buildRequestMysign($para_sort)
+    public function buildRequestMysign($params)
     {
+        $para_filter = StringUtils::paraFilter($params, array('sign'));
+        $para_sort = StringUtils::argSort($para_filter);
         $string = StringUtils::createLinkstring($para_sort);
         $result = strtoupper(md5($string . "&key=" . $this->config['key']));
         return $result;
     }
 
+    /**
+     * 验签
+     */
+    public function CheckSign($params)
+    {
+        if (! isset($params['sign']) || ! $params['sign']) {
+            return false;
+        }
+        $sign = $this->buildRequestMysign($params);
+        if ($params['sign'] != $sign) {
+            return false;
+        }
+        return true;
+    }
+    
+    
     /**
      * 以post方式提交xml到对应的接口url
      *
@@ -140,8 +147,69 @@ class WxNative extends PayBase
     }
 
     public function back()
-    {}
+    {
+        return true;
+    }
 
     public function notify()
-    {}
+    {
+        $xml = file_get_contents("php://input");
+        
+        $msg = "OK";
+        $result = true;
+        
+        $data = StringUtils::XmlToArray($xml);
+        
+        if ($data['return_code'] != 'SUCCESS') {
+            return false;
+        }
+        if (! $this->CheckSign($data)) {
+            return false;
+        }
+        
+        if(!array_key_exists("transaction_id", $data)){
+            return false;
+        }
+        $input['transaction_id'] = $data["transaction_id"];
+        
+        if(self::orderQuery($input) == false){
+            return false;
+        }
+        
+        if($data['result_code'] == 'FAIL'){
+            return false;
+        }
+        
+        return array(
+            'out_trade_no' => $data['out_trade_no'],
+            'data' => $data
+        );
+    }
+    
+    /**
+     * 查询订单，WxPayOrderQuery中out_trade_no、transaction_id至少填一个
+     * appid、mchid、spbill_create_ip、nonce_str不需要填入
+     */
+    public function orderQuery($input)
+    {
+        if(!isset($input['out_trade_no']) && !isset($input['transaction_id'])){
+            return false;
+        }
+        $input['appid']   = $this->config['appid'];
+        $input['mch_id']  = $this->config['mch_id'];
+        $input['nonce_str'] = StringUtils::createNonceString();
+        
+        $input['sign'] = $this->buildRequestMysign($input);
+        
+        $xml = StringUtils::arrayToXml($input);
+    
+        $response = $this->postXmlCurl($xml, self::QUERY_ORDER_URL);
+        
+        $result = StringUtils::XmlToArray($response);
+    
+        if(!array_key_exists("return_code", $result) || !array_key_exists("result_code", $result) || $result["return_code"] != "SUCCESS" || $result["result_code"] != "SUCCESS")
+            return false;
+    
+        return true;
+    }
 }
